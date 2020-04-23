288fb60711b2033a86b1b20e8eb2a11aba70a989
Mario Toscano <toscanomario97@gmail.com>
2020-04-23 13:30:09 +0200

    aggiunta script nemici follow e loop

---
+++ Prova/Assets/Script/EnemyStaSulCoso.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStaSulCoso : MonoBehaviour
+{
+    public int speed;
+    public float distance;
+    public Transform groundDetection;
+
+    private bool movingRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movingRight = true;       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right* speed*Time.deltaTime);
+        RaycastHit2D groundInfo=Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+       if(groundInfo.collider==false)
+        {
+            print("trovato collissione");
+            if (movingRight==true)
+            {
+                print("qui entro");
+                transform.eulerAngles= new Vector3(0, -180, 0);
+                movingRight = false;
+            }else
+            {
+                print("tua mamma");
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
+}
---
+++ Prova/Assets/Script/NemicoCheTiSegue.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class NemicoCheTiSegue : MonoBehaviour
+{
+    public int speed;
+    private Transform target;
+    public float stoppingDistance;
+    public Transform GroundDetection;
+    private bool movingRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movingRight = true;
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D groundInfo = Physics2D.Raycast(GroundDetection.position, Vector2.down, 0.2f);
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            if (groundInfo.collider == true)
+            {
+                if (transform.position.x - target.position.x<0)
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                }else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                }
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            }
+            else
+            {
+                if (movingRight)
+                {
+                    transform.eulerAngles = new Vector3(0, -180, 0);
+                    movingRight = false;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    movingRight = true;
+                }
+
+
+            }
+        }
+    }
+}
